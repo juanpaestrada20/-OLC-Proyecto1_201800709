c7c440ecfc92debd6160a631807be9c297f3a7af
Juan Pablo Estrada <3599546500101@ingenieria.usac.edu.gt>
2020-03-06 10:37:50 -0600

    Inicio Sintactico

---
+++ Compi1_Proyecto1/AnalizadorSintactico.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compi1_Proyecto1
+{
+  class AnalizadorSintactico
+  {
+    LinkedList<Token> salidaSintactico;
+    LinkedList<Token> salidaErrores;
+
+    public LinkedList<Token> verificarTokens (LinkedList<Token> tokens)
+    {
+      return salidaSintactico;
+    }
+  }
+}
